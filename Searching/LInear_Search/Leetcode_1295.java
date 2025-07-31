809952956789f377858de9c25d1690f3d57875c1
Poojitha-0508 <attilipooja@gmail.com>
2025-07-31 20:16:54 +0530

    Create Leetcode_1295.java

---
+++ Searching/LInear_Search/Leetcode_1295.java
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/find-numbers-with-even-number-of-digits/description/
+
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner in = new Scanner(System.in);
+	    int n=in.nextInt();
+	    int[] arr = new int[n];
+	    for (int i=0;i<n;i++){
+	        arr[i]=in.nextInt();
+	    }
+	    System.out.println(findcount(arr));
+	}
+	static int findcount(int[] arr){
+	    int count=0;
+	    for(int num:arr){
+	        if(findeven(num)){
+	            count++;
+	        }
+	    }
+	    return count;
+	}
+	static boolean findeven(int num){
+	    int c=0;
+	    while(num>0){
+	        c++;
+	        num/=10;
+	    }
+	    return c%2==0;
+	}
+}
