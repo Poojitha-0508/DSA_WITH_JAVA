8243e72c9e0e6a0ecb1e8faeb025519578d34303
Poojitha-0508 <attilipooja@gmail.com>
2025-08-15 20:04:25 +0530

    Create AllMissingNumbers.java

---
+++ Sorting/CycleSort_With_probs/Problems./AllMissingNumbers.java
@@ -0,0 +1,39 @@
+// https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
+
+
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner in=new Scanner(System.in);
+	    int n=in.nextInt();
+	    int[] arr=new int[n];
+	    for(int i=0;i<n;i++){
+	        arr[i]=in.nextInt();
+	    }
+	    
+		System.out.println(MissingNumber(arr));
+	}
+	static ArrayList<Integer> MissingNumber(int[] arr){
+	    int i=0;
+	    while(i<arr.length){
+	        int correct=arr[i]-1;
+	        if(arr[i]!=arr[correct]){
+	            int temp=arr[i];
+	            arr[i]=arr[correct];
+	            arr[correct]=temp;
+	        }
+	        else{
+	            i++;
+	        }
+	    }
+	    ArrayList<Integer> list = new ArrayList<>();
+	    for(i=0;i<arr.length;i++){
+	        if(i+1!=arr[i]){
+	            list.add(i+1);
+	        }
+	    }
+	    return list;
+	}
+}
