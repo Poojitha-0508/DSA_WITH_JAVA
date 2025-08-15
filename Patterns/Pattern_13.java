e7b5a8ceeae5b76dfbe0aeff13e855a8b4a690eb
Poojitha-0508 <attilipooja@gmail.com>
2025-08-15 09:06:03 +0530

    Create Pattern_13.java

---
+++ Patterns/Pattern_13.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner in =new Scanner(System.in);
+		int n=in.nextInt();
+        for(int i=1;i<=n;i++){
+            for (int j=1;j<=(2*n)-1;j++){
+                if (i==n){
+                    System.out.print("*");
+                }
+                else{
+                    if (j<=n){
+                        if (i+j==n+1){
+                            System.out.print("*");
+                        }
+                        else{
+                            System.out.print(" ");
+                        }
+                    }
+                    else{
+                        if ((j-n+1)==i){
+                            System.out.print("*");
+                        }
+                        else{
+                            System.out.print(" ");
+                        }
+                    }
+                }
+            }
+            System.out.println ();
+        }
+    }
+}
