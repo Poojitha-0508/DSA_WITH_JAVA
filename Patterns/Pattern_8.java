3f099af05d157f6e911d97439d26ce40b112223b
Poojitha-0508 <attilipooja@gmail.com>
2025-08-16 09:31:20 +0530

    Create Pattern_8.java

---
+++ Patterns/Pattern_8.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner in =new Scanner(System.in);
+		int n=in.nextInt();
+        for(int i=1;i<=n;i++){  // n rows
+            for (int j=1;j<=(2*n)-1;j++){  //2n-1 cols 
+                if (i==n || j==n){
+                    System.out.print("*");
+                }
+                else{
+                    if (j<=n){
+                        if (i+j>n){
+                            System.out.print("*");
+                        }
+                        else{
+                            System.out.print(" ");
+                        }
+                    }
+                    else{
+                        if ((j-n)<i){
+                            System.out.print("*");
+                        }
+                        else{
+                            System.out.print(" ");
+                        }
+                    }
+                }
+            }
+            System.out.println ();
+        }
+    }
+}
